42d7ee85f1f96bef626a9bebcf3f416e513e1a4c
Shibly Meeran <git@shib.me>
2019-01-30 23:20:57 +0530

    adding a mini jira formatter for commonmark spec

--- src/me/shib/bugaudit/commons/MarkdownContent.java
+++ src/me/shib/bugaudit/commons/MarkdownContent.java
@@ -11,4 +11,17 @@ public class MarkdownContent {
     public String getMarkdownText() {
         return markdownText;
     }
+
+    public String getJiraFormatText() {
+        String jiraText = markdownText.replace("**", "*").replace("__", "*");
+        jiraText = jiraText.replace("```", "{code}")
+                .replace("~~~", "{code}");
+        String[] urlSplit = jiraText.split("]\\(");
+        StringBuilder urlCleanedText = new StringBuilder().append(urlSplit[0]);
+        for (int i = 1; i < urlSplit.length; i++) {
+            urlCleanedText.append("|").append(urlSplit[i].replaceFirst("\\)", "]"));
+        }
+        return urlCleanedText.toString();
+    }
+
 }
